b1304f3f136d127a2bea968f856df11c37476e22
gucc <2046237960@qq.com>
2023-09-17 23:39:54 +0800

    第一次初始化

---
+++ src/main/java/com/gcc/javacoreday01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.gcc.javacoreday01;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("你好 java");
+        System.out.println("你好 java");
+        System.out.println("你好 java");
+        System.out.println("你好 java");
+        System.out.println("你好 java");
+
+    }
+}
