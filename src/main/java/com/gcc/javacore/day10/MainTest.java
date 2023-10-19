01939443b03d10050de662e15c9d345a7317f8a4
gucc <2046237960@qq.com>
2023-10-19 13:25:25 +0800

    day10

---
+++ src/main/java/com/gcc/javacore/day10/MainTest.java
@@ -0,0 +1,23 @@
+package com.gcc.javacore.day10;
+
+public class MainTest {
+    public static void main(String[] args) {
+//        System.out.println("第1遍，好好学习，天天向上");
+//        System.out.println("第2遍，好好学习，天天向上");
+//        System.out.println("第3遍，好好学习，天天向上");
+//        System.out.println("第4遍，好好学习，天天向上");
+//        System.out.println("第5遍，好好学习，天天向上");
+//        System.out.println("第6遍，好好学习，天天向上");
+//        System.out.println("第7遍，好好学习，天天向上");
+//        System.out.println("第8遍，好好学习，天天向上");
+//        System.out.println("第9遍，好好学习，天天向上");
+//        System.out.println("第10遍，好好学 习，天天向上");
+
+        int i = 1;//计数器
+        while(i<=100){
+            System.out.println("第"+i+"遍，好好学习，天天向上");
+            i++;
+        }
+
+    }
+}
