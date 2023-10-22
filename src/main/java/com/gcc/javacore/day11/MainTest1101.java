70ec11aac920cf266c3deb185929d9aea3525575
gucc <2046237960@qq.com>
2023-10-22 16:05:01 +0800

    day11

---
+++ src/main/java/com/gcc/javacore/day11/MainTest1101.java
@@ -0,0 +1,22 @@
+package com.gcc.javacore.day11;
+
+import java.util.Scanner;
+
+public class MainTest1101 {
+    public static void main(String[] args) {
+        //循环录入10个学生的成绩，统计成绩大于等于80分的学生比例
+        int num = 0;//计数器用来统计超过80分的个数
+        for (int i = 0; i < 10; i++) {
+            System.out.println("请输入第" + (i + 1) + "个学生成绩");
+            Scanner scanner = new Scanner(System.in);
+            int score = scanner.nextInt();
+            if (score >= 80) {
+                num++;
+                continue;
+            }
+            System.out.println(score + "continue测试");
+        }
+        int bi = num/10;
+        System.out.println("80分占比:"+bi);
+    }
+}
