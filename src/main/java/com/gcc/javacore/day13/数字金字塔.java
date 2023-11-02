104ccf7fd3c15a7a6845012ba9a3f34b7b704fa9
gucc <2046237960@qq.com>
2023-11-02 10:56:29 +0800

    day13

---
+++ src/main/java/com/gcc/javacore/day13/数字金字塔.java
@@ -0,0 +1,22 @@
+package com.gcc.javacore.day13;
+
+import java.util.Scanner;
+
+public class 数字金字塔 {
+    public static void main(String[] args) {
+        System.out.println("请输入行号:");
+        Scanner scanner =new Scanner(System.in);
+        int num= scanner.nextInt();
+        //外层循环
+        for (int i=0;i<num;i++){
+            for (int j=0;j<num-i;j++){
+                System.out.print(" ");
+            }
+            //1，3，5，7，9
+            for (int j=0;j<i*2+1;j++){
+                System.out.print(i+1);
+            }
+            System.out.println("");
+        }
+    }
+}
