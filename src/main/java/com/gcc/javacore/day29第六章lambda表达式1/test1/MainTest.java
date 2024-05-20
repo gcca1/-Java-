670d811132944045bc872f7415ce23b10984a0f0
gucc <2046237960@qq.com>
2024-05-20 12:11:35 +0800

    day29

---
+++ src/main/java/com/gcc/javacore/day29第六章lambda表达式1/test1/MainTest.java
@@ -0,0 +1,9 @@
+package com.gcc.javacore.day29第六章lambda表达式1.test1;
+
+public class MainTest {
+    public static void main(String[] args) {
+        String[] arrStr = {"aaa","cccc","bbbbb","eeee","dd"};
+         
+    }
+}
+  
